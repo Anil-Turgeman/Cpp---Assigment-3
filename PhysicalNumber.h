9af46c45a56a51bfc7ad1800a7c4611998ba522c
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-08 13:14:22 +0300

    adding function prototypes

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -1,9 +1,45 @@
+#ifndef PhysicalNumber_H
+#define PhysicalNumber_H
+
 #include <iostream>
+#include <string>
 #include "Unit.h"
-using std::istream, std::ostream, std::istringstream;
-namespace ariel{
+using namespace std;
 
+namespace ariel {
+    class PhysicalNumber{
+        private:
+            Unit unit;
+            double data;
 
+        public:
+            PhysicalNumber(double, Unit);
+            double getData();
+            Unit getUnit();
+            void setData(double);
+            void setUnit(Unit);
+            
+            const PhysicalNumber operator+();
+            const PhysicalNumber& operator++();       //prefix
+            const PhysicalNumber& operator++(int);    //postfix
+            const PhysicalNumber operator+=(const PhysicalNumber&);
+            friend const PhysicalNumber operator+(PhysicalNumber&, const PhysicalNumber&);
+            const PhysicalNumber operator-();
+            const PhysicalNumber& operator--();        //prefix
+            const PhysicalNumber& operator--(int);     //postfix
+            const PhysicalNumber operator-=(const PhysicalNumber&);
+            friend const PhysicalNumber operator-(const PhysicalNumber&, const PhysicalNumber&);
 
-
+            bool operator==(const PhysicalNumber&);
+            bool operator!=(const PhysicalNumber&);
+            bool operator>(const PhysicalNumber&);
+            bool operator<(const PhysicalNumber&);
+            bool operator<=(const PhysicalNumber&);
+            bool operator>=(const PhysicalNumber&);
+        
+            friend ostream& operator<<(ostream&, const PhysicalNumber&);
+            friend istream& operator>>(istream&, PhysicalNumber&);    
+    };
 }
+
+#endif
