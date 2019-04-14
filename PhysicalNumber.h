6702901c90286ffe96dffac080843462824be859
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-14 20:45:24 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -20,13 +20,13 @@ namespace ariel {
             void setUnit(Unit);
             
             const PhysicalNumber operator+();
-            const PhysicalNumber& operator++();       //prefix
-            const PhysicalNumber& operator++(int);    //postfix
+            const PhysicalNumber& operator++();//prefix opertor -> ++i
+            const PhysicalNumber& operator++(int);//postfix opertor -> i++
             const PhysicalNumber operator+=(const PhysicalNumber&);
             friend const PhysicalNumber operator+(PhysicalNumber&, const PhysicalNumber&);
             const PhysicalNumber operator-();
-            const PhysicalNumber& operator--();        //prefix
-            const PhysicalNumber& operator--(int);     //postfix
+            const PhysicalNumber& operator--();//prefix opertor -> ++i
+            const PhysicalNumber& operator--(int);//postfix opertor -> i++
             const PhysicalNumber operator-=(const PhysicalNumber&);
             friend const PhysicalNumber operator-(const PhysicalNumber&, const PhysicalNumber&);
 
@@ -38,7 +38,7 @@ namespace ariel {
             bool operator>=(const PhysicalNumber&);
         
             friend ostream& operator<<(ostream&, const PhysicalNumber&);
-            friend istream& operator>>(istream&, PhysicalNumber&);    
+            friend istream& operator>>(istream&, PhysicalNumber&);
     };
 }
 
