fd6b9ed7b8335a6fdf03bc46b35ee98491c336b2
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-16 15:06:10 +0300

    Update PhysicalNumber.cpp

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -1,124 +1,296 @@
+#include <sstream>
 #include "PhysicalNumber.h"
-#include <iostream>
+#include "Unit.h"
+
+using namespace std;
 using namespace ariel;
 
-PhysicalNumber::PhysicalNumber(double x, Unit u) : data(x), unit(u) {}
 
-double PhysicalNumber::getData() {
-    return data;
+PhysicalNumber::PhysicalNumber(double data, Unit u):data(data), unit(u){}
+
+double PhysicalNumber::getData(){   return data;    }
+
+void PhysicalNumber::setData(double other){ data = other;   }
+
+Unit PhysicalNumber::getUnit(){ return unit;    }
+
+void PhysicalNumber::setUnit(Unit u){   unit = u;   }
+
+PhysicalNumber PhysicalNumber::operator+(const PhysicalNumber &other) const{
+    double res = convert(*this, other); 
+    return PhysicalNumber(this->data + res, this->unit);
 }
 
-Unit PhysicalNumber::getUnit() {
-    return unit;
+PhysicalNumber PhysicalNumber::operator-(const PhysicalNumber &other) const{
+    double res = convert(*this, other); 
+    return PhysicalNumber(this->data - res, this->unit);
 }
 
-void PhysicalNumber::setData(double d) {
-    data = d;
-}
 
-void PhysicalNumber::setUnit(Unit u) {
-    unit = u;
-}
+PhysicalNumber PhysicalNumber::operator-() const { return PhysicalNumber((-1)*data, unit); }//the unary operator
 
-const PhysicalNumber PhysicalNumber::operator+() {
-    return PhysicalNumber(data, unit);
-}
+PhysicalNumber PhysicalNumber::operator+() const { return PhysicalNumber(1*data, unit); }//the unary operator
 
-const PhysicalNumber& PhysicalNumber::operator++() {
+
+PhysicalNumber &PhysicalNumber::operator+=(const PhysicalNumber &other){
+    double res = convert(*this, other); 
+    this->data = this->data + res;
     return *this;
 }
 
-const PhysicalNumber& PhysicalNumber::operator++(int unused) {
+PhysicalNumber &PhysicalNumber::operator-=(const PhysicalNumber &other){
+    double res = convert(*this, other); 
+    this->data = this->data - res;
+    return *this;
+
+}
+
+bool PhysicalNumber::operator==(const PhysicalNumber &value) const{
+    double number = 0;
+    try { number = convert(*this, value); }
+    catch (string e) { cerr << "Exception: Error in convert" << endl; }
+    return this->data == number;
+}
+
+bool PhysicalNumber::operator!=(const PhysicalNumber &value) const{
+    double number = 0;
+    try { number = convert(*this, value); }
+    catch (string e) { cerr << "Exception: Error in convert" << endl; }
+    return this->data != number;
+}
+
+
+PhysicalNumber &PhysicalNumber::operator++(){
+    this->data++;
     return *this;
 }
 
-const PhysicalNumber PhysicalNumber::operator+=(const PhysicalNumber& other) {
-    return PhysicalNumber(0.0,unit);
-}
-
-const PhysicalNumber ariel::operator+(PhysicalNumber& left, const PhysicalNumber& right) {
-    return PhysicalNumber(0.0, Unit::KM);
-}
-
-const PhysicalNumber PhysicalNumber::operator-() {
-    return PhysicalNumber(-data, unit);
-}
-
-const PhysicalNumber& PhysicalNumber::operator--() {
+PhysicalNumber &PhysicalNumber::operator--(){
+    this->data--;
     return *this;
 }
 
-const PhysicalNumber& PhysicalNumber::operator--(int unused) {
-    return *this;
+PhysicalNumber PhysicalNumber::operator++(int){
+    PhysicalNumber temp(*this);
+    temp.getData->data++;
+    return temp;
 }
 
-const PhysicalNumber PhysicalNumber::operator-=(const PhysicalNumber& other) {
-    return PhysicalNumber(0.0,unit);
+PhysicalNumber PhysicalNumber::operator--(int){
+    PhysicalNumber temp(*this);
+    temp.getData->data--;
+    return temp;
 }
 
-const PhysicalNumber ariel::operator-(const PhysicalNumber& left, const PhysicalNumber& right) { 
-    return PhysicalNumber(0.0, left.unit);
+
+double PhysicalNumber::convert(const PhysicalNumber &first, const PhysicalNumber &second) const{
+    Unit first_unit = first.unit;
+    Unit r_unit = second.unit;
+    
+    double l_val = first.data;
+    double r_val = second.data;
+
+    double result = 0;
+
+    switch (r_unit){
+        case Unit::KM:
+
+            if (first_unit == Unit::M) { result = r_val * 1000; }
+
+            else if (first_unit == Unit::CM) { result = r_val * 100000; }
+
+            else if (first_unit == Unit::KM) { result = r_val * 1; }
+
+            else { cerr << "can't convert" << endl; }
+
+            break;
+
+        case Unit::M:
+
+            if (first_unit == Unit::CM) { result = r_val * 100; }
+
+            else if (first_unit == Unit::KM) { result = r_val / 1000; }
+
+            else if (first_unit == Unit::M) { result = r_val * 1; }
+
+            else { cerr << "can't convert" << endl; }
+
+            break;
+
+        case Unit::CM:
+
+            if (first_unit == Unit::M) { result = r_val / 100; }
+
+            else if (first_unit == Unit::KM) { result = r_val / 100000; }
+
+            else if (first_unit == Unit::CM) { result = r_val * 1; }
+
+            else { cerr << "can't convert" << endl; }
+
+        break;
+
+        case Unit::HOUR:
+
+            if (first_unit == Unit::MIN) { result = r_val * 60; }
+            
+            else if (first_unit == Unit::SEC) { result = r_val * 3600; }
+            
+            else if (first_unit == Unit::HOUR) { result = r_val * 1; }
+            
+            else { cerr << "can't convert" << endl; }
+            
+            break;
+
+        case Unit::MIN:
+
+            if (first_unit == Unit::HOUR) { result = r_val / 60; }
+            
+            else if (first_unit == Unit::SEC) { result = r_val * 60; }
+            
+            else if (first_unit == Unit::MIN) { result = r_val * 1; }
+            
+            else { cerr << "can't convert" << endl; }
+            
+            break;
+
+        case Unit::SEC:
+
+            if (first_unit == Unit::MIN) { result = r_val / 60; }
+            
+            else if (first_unit == Unit::HOUR) { result = r_val / 3600; }
+            
+            else if (first_unit == Unit::SEC) { result = r_val * 1; }
+            
+            else { cerr << "can't convert" << endl; }
+            
+            break;
+
+        case Unit::TON:
+
+            if (first_unit == Unit::KG) { result = r_val * 1000; }
+            
+            else if (first_unit == Unit::G) { result = r_val * 1000000; }
+            
+            else if (first_unit == Unit::TON) { result = r_val * 1; }
+            
+            else { cerr << "can't convert" << endl; }
+            
+            break;
+
+        case Unit::KG:
+
+            if (first_unit == Unit::G) { result = r_val * 1000; }
+            
+            else if (first_unit == Unit::TON) { result = r_val / 1000; }
+            
+            else if (first_unit == Unit::KG) { result = r_val * 1; }
+            
+            else { cerr << "can't convert" << endl; }
+            
+            break;
+
+        case Unit::G:
+
+            if (first_unit == Unit::TON) { result = r_val / 1000000; }
+            
+            else if (first_unit == Unit::KG) { result = r_val / 1000; }
+            
+            else if (first_unit == Unit::G) { result = r_val * 1; }
+            
+            else { cerr << "can't convert" << endl; }
+            
+            break;
+
+        default:
+
+            cerr << "can't convert" << endl;
+        }
+    return result;
 }
 
-bool PhysicalNumber::operator==(const PhysicalNumber& other) {
-    return false;
+bool PhysicalNumber::operator<(const PhysicalNumber &value) const{
+    double number = 0;
+    try { number = convert(*this, value); }
+    catch (string e) { cerr << "Exception: Error in convert" << endl; }
+    return this->data < number;
 }
 
-bool PhysicalNumber::operator!=(const PhysicalNumber& other) {
-    return false;
+bool PhysicalNumber::operator>(const PhysicalNumber &value) const{
+    double number = 0;
+    try { number = convert(*this, value); }
+    catch (string e) { cerr << "Exception: Error in convert" << endl; }
+    return this->data > number;
 }
 
-bool PhysicalNumber::operator>(const PhysicalNumber& other) {
-    return false;
-} 
-
-bool PhysicalNumber::operator<(const PhysicalNumber& other) {
-    return false;
+bool PhysicalNumber::operator<=(const PhysicalNumber &value) const{
+    double number = 0;
+    try { number = convert(*this, value); }
+    catch (string e) { cerr << "Exception: Error in convert" << endl; }
+    return this->data <= number;
 }
 
-bool PhysicalNumber::operator<=(const PhysicalNumber& other) {
-    return false;
+bool PhysicalNumber::operator>=(const PhysicalNumber &value) const{
+    double number = 0;
+    try { number = convert(*this, value); }
+    catch (string e) { cerr << "Exception: Error in convert" << endl; }
+    return this->data >= number;
 }
 
-bool PhysicalNumber::operator>=(const PhysicalNumber& other) {
-    return false;
-}
 
-ostream& ariel::operator<<(ostream& os, const PhysicalNumber& pn) {
-    string str;
-    switch (pn.unit) {
-        case Unit::KM :
-            str = "km";
-            break;
-        case Unit::M :
-            str = "m";
-            break;
-        case Unit::CM :
-            str = "cm";
-            break;
-        case Unit::HOUR :
-            str = "hour";
-            break;
-        case Unit::MIN :
-            str = "min";
-            break;
-        case Unit::SEC :
-            str = "sec";
-            break;
-        case Unit::TON :
-            str = "ton";
-            break;
-        case Unit::KG :
-            str = "kg";
-            break;
-        case Unit::G :
-            str = "g";
-            break;
-    }
-    return os << pn.data << "[" << str << "]";
-}
+//cin stream operator
 
-istream& ariel::operator>>(istream& is, PhysicalNumber& pn) {
-    is >> pn.data;
+istream& ariel::operator>>(istream &is, PhysicalNumber &p){
     return is;
+}
+
+
+//cout stream operator
+ostream& ariel::operator<<(ostream &os, const PhysicalNumber &p){
+    switch (p.unit){
+        case Unit::KM:{
+            os << p.data << "[km]";
+            break;
+        }
+
+        case Unit::M:{
+            os << p.data << "[m]";
+            break;
+        }
+
+        case Unit::CM:{
+            os << p.data << "[cm]";
+            break;
+        }
+
+        case Unit::HOUR:{
+            os << p.data << "[hour]";
+            break;
+        }
+
+        case Unit::MIN:{
+            os << p.data << "[min]";
+            break;
+        }
+
+        case Unit::SEC:{
+            os << p.data << "[sec]";
+            break;
+        }
+
+        case Unit::TON:{
+            os << p.data << "[ton]";
+            break;
+        }
+
+        case Unit::KG:{
+            os << p.data << "[kg]";
+            break;
+        }
+
+        case Unit::G:{
+            os << p.data << "[g]";
+            break;
+        }
+    }
+    return os;
 }
